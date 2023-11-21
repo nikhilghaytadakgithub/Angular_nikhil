21e1989c156a494c38b0b1f50d1fddb71de73dac
nikhilghaytadakgithub <ghaytadaknikhil@gmail.com>
2023-11-21 12:35:29 +0530

    assignment 1 complted

--- angularPipe/src/app/app.component.ts
+++ angularPipe/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Employee } from 'src/model/employe';
+import { EmpGadgetPipe } from './emp-gadget.pipe';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularPipe';
+ 
+  public  empsArray:Employee[]  =  [
+    {  empno : 10256, ename:"Scott", job: "Manager", deptno : 10, grade: 1  },
+    {  empno : 10257, ename:"Smith", job: "Lead", deptno : 20, grade: 2  },
+    {  empno : 10258, ename:"Sandy", job: "Programmer", deptno : 30, grade: 3  },
+    {  empno : 10259, ename:"Sam", job: "Tester", deptno : 40, grade: 4  } ,
+    {  empno : 10256, ename:"Scott", job: "Manager", deptno : 10, grade: 1  },
+    {  empno : 10257, ename:"Smith", job: "Lead", deptno : 20, grade: 2  },
+    {  empno : 10258, ename:"Sandy", job: "Programmer", deptno : 30, grade: 3  },
+    {  empno : 10259, ename:"Sam", job: "Tester", deptno : 40, grade: 4  } ,
+    {  empno : 10256, ename:"Scott", job: "Manager", deptno : 10, grade: 1  },
+    {  empno : 10257, ename:"Smith", job: "Lead", deptno : 20, grade: 2  },
+    {  empno : 10258, ename:"Sandy", job: "Programmer", deptno : 30, grade: 3  },
+    {  empno : 10259, ename:"Sam", job: "Tester", deptno : 40, grade: 4  } 
+  ];	
 }
--- angularPipe/src/app/app.module.ts
+++ angularPipe/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmpGadgetPipe } from './emp-gadget.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpGadgetPipe
   ],
   imports: [
     BrowserModule
