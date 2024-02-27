852b6775ba6d5d17afadbdeeb695182ed6456a1e
nikhilghaytadakgithub <ghaytadaknikhil@gmail.com>
2024-02-27 11:28:40 +0530

    test othing

---
+++ Day4_Assignment2/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { VehicleRegistrationComponent } from './vehicle-registration/vehicle-registration.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    VehicleRegistrationComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Day4_Assignment2/src/app/vehicle-registration/vehicle-registration.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-registration',
+  templateUrl: './vehicle-registration.component.html',
+  styleUrls: ['./vehicle-registration.component.css']
+})
+export class VehicleRegistrationComponent {
+
+}
